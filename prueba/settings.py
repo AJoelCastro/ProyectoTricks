e6f3f342b4da66d4e0b497ce1563c1c5cba54638
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-04-02 23:43:05 -0500

    conexion con mysql

--- prueba/settings.py
+++ prueba/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'prueba.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dbTricks',
-        'USER': 'postgres',
+        'USER': 'root',
         'PASSWORD': 'Cnic2024#kiki',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '3306',
     }
 }
 
