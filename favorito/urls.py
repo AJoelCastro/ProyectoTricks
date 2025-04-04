9d895780c8bb49a4573d32c34de3245f0f83f38c
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-04-04 00:07:11 -0500

    favorites

--- favorito/urls.py
+++ favorito/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('menu_favoritos/', views.menu_favoritos, name="menu_favoritos"),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('<int:product_id>/', views.add_to_favorites, name='add_to_favorites'),
+    path('<int:product_id>/', views.remove_from_favorites, name='remove_from_favorites'),
 ]
--- favorito/views.py
+++ favorito/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Favorite
+from Product.models import Product
 
 @login_required
 def menu_favoritos(request):
-    return render(request, 'menu_favoritos.html', {})
+    favorites = Favorite.objects.filter(user=request.user)
+    return render(request, 'menu_favoritos.html', {'favorites': favorites})
+
+def add_to_favorites(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    Favorite.objects.create(user=request.user, product=product)
+    return redirect('menu_favoritos')
+
+def remove_from_favorites(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    Favorite.objects.filter(user=request.user, product=product).delete()
+    return redirect('menu_favoritos')
 
