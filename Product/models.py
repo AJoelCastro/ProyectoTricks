61307714041121d04dd15220e1c20bc14045253a
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-04-03 23:47:12 -0500

    modificando favorites

--- Product/models.py
+++ Product/models.py
@@ -39,13 +39,3 @@ class ProductImage(models.Model):
         return f"Imagen de {self.product.name}"
     
     
-class Favorite(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ('user', 'product')  # Evita duplicados
-
-    def __str__(self):
-        return f"{self.user.username} - {self.product.name}"
