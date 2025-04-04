8789d5053966e3368d6a50c6970a790e713549ad
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-04-03 22:49:09 -0500

    images

--- Product/admin.py
+++ Product/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product, Favorite, Color
+from .models import Product, Favorite, Color, ProductImage
 
 admin.site.register(Product)
 admin.site.register(Favorite)
 admin.site.register(Color)
+admin.site.register(ProductImage)
 
 # Register your models here.
--- Product/models.py
+++ Product/models.py
@@ -23,14 +23,22 @@ class Product(models.Model):
     description = models.TextField(max_length=255, blank=False, null=False)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
     color = models.ManyToManyField(Color)
-    image = models.URLField(blank=False, null=False)
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='tendencias', blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+    
+    
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")  # Relación 1 a N
+    image_url = models.URLField(blank=False, null=False)
 
+    def __str__(self):
+        return f"Imagen de {self.product.name}"
+    
+    
 class Favorite(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
