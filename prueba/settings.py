6aaaf31411b94f847aa936eb22069363328f6f55
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-04-23 18:13:23 -0500

    SECRET

--- prueba/settings.py
+++ prueba/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-wdixapcvyxxtz(!-+63w2i!jpidk_!&kf*!lk@#ijg&9c)pygb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
