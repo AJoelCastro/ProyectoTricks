60e661a2e0b159ef6ad511aba4953262ed5bfe33
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-04-23 23:45:53 -0500

    changes

--- prueba/settings.py
+++ prueba/settings.py
@@ -16,8 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
-
+ALLOWED_HOSTS = [".onrender.com",".railway.app","127.0.0.1","localhost"]
 
 # Application definition
 
